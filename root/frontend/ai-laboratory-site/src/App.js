86609655741556ddd8be448dcbe3ae1d61167205
Amur <amur_almukhametov@mail.ru>
2021-11-20 17:40:59 +0500

    Роутинг + авторизация + регистрация (стили не до конца)

--- root/frontend/ai-laboratory-site/src/App.js
+++ root/frontend/ai-laboratory-site/src/App.js
@@ -1,19 +1,26 @@
-import './App.css';
+/*import './App.css';*/
 import React from "react";
 import Footer from "./Components/Footer/footer";
 import Header from "./Components/Header/header";
 import Information from "./Components/Information/information";
 import Partners from "./Components/Partners/partners";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Landing from "./Components/landing";
+import Login from "./Components/Login/login";
+import Registration from "./Components/Registration/registration";
 
 
 function App() {
   return (
-    <div className="App">
-        <Header />
-        <Information/>
-        <Partners/>
-        <Footer />
-    </div>
+      <BrowserRouter>
+          <div className="App">
+              <Routes>
+                  <Route path="/" element={<Landing />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/registration" element={<Registration />} />
+              </Routes>
+          </div>
+      </BrowserRouter>
   );
 }
 

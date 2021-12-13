d3b1bd1c39bc3d04c77c2589b8480200a5239ffd
mybec <my.beck.for.work@mail.ru>
2021-12-13 21:32:54 +0500

    Исправил чтение данных request-а, проверил работу api /login и /reg

--- root/backend/site/backend_server.py
+++ root/backend/site/backend_server.py
@@ -9,21 +9,24 @@ def start_server():
 
     @routes.post('/api/login')
     async def log_in(request):
-        server_data = await request.post()
-        login = server_data.get('email')
-        password = server_data.get('pass')
+        server_data = await request.json()
+        login = server_data['email']
+        password = server_data['pass']
         if login and password:
+            print('data was delivered')
             user = Users().find(User(password=password, email=login))
             if user.id:
                 return web.Response(status=200, text=json.dumps({'user_id': user.id, 'status': 10}))
-        return web.Response(status=200, text=json.dumps({'user_id': None, 'status': 0}))
+
+        bad_json = {'status': 120}
+        return web.json_response(bad_json)
 
     @routes.post('/api/reg')
     async def sign_in(request):
-        server_data = await request.post()
-        login = server_data.get('email')
-        password = server_data.get('pas')
-        full_name = server_data.get('name')
+        server_data = await request.json()
+        login = server_data['email']
+        password = server_data['pass']
+        full_name = server_data['name']
         if login and password and full_name:
             new_user = User(email=login, password=password, full_name=full_name)
             user = Users().find(User(email=login))

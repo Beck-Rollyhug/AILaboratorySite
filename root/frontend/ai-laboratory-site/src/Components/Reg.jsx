b03f6c58b3f87b09008f71f716b4238dd0a8ab63
mybec <my.beck.for.work@mail.ru>
2022-01-15 22:57:02 +0500

    Auth 2, Методы для регистрации

--- root/frontend/ai-laboratory-site/src/Components/Reg.jsx
+++ root/frontend/ai-laboratory-site/src/Components/Reg.jsx
@@ -1,14 +1,39 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import './Styles/Reg.css'
 import {Link} from "react-router-dom";
 import {AuthContext} from "../context";
 
+async function Auth(credentials) {
+    let response = await fetch('/api/reg',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        })
+        .then(data => data.json());
+    console.log(response);
+}
+
 const Reg = () => {
     const {isAuth, setIsAuth} = useContext(AuthContext);
-    const login = () => {
+
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [full_name, setFullName] = useState();
+
+    const HandleForm = async e => {
+        e.preventDefault();
+        await Auth(
+            {
+                'email': email,
+                'password': password,
+                'full_name': full_name
+            }
+        );
         setIsAuth(true);
         localStorage.setItem('auth', 'true');
-
     }
     return (
         <main className="formSignin">
@@ -16,15 +41,15 @@ const Reg = () => {
                 {/*<img className="logoMenu" src="img-ds/IIS%20logo.png" alt="iis-logo"/>*/}
                     <h2 className="title">Регистрация</h2>
                     <div className="formFloating">
-                        <input type="text" className="formControl" id="floatingName" placeholder="ФИО..."/>
+                        <input type="text" onChange={event => setFullName(event.target.value)} className="formControl" id="floatingName" placeholder="ФИО..."/>
                             <label htmlFor="floatingName"></label>
                     </div>
                     <div className="formFloating">
-                        <input type="email" className="formControl" id="floatingInput" placeholder="Почта..."/>
+                        <input type="email" onChange={event => setEmail(event.target.value)} className="formControl" id="floatingInput" placeholder="Почта..."/>
                             <label htmlFor="floatingInput"></label>
                     </div>
                     <div className="formFloating">
-                        <input type="password" className="formControl" id="floatingPassword" placeholder="Пароль..."/>
+                        <input type="password" onChange={event => setPassword(event.target.value)} className="formControl" id="floatingPassword" placeholder="Пароль..."/>
                             <label htmlFor="floatingPassword"></label>
                     </div>
                     <div className="formCheck">
@@ -33,7 +58,7 @@ const Reg = () => {
                                 Я сейчас учусь
                             </label>
                     </div>
-                    <button className="btn" onClick={login} type="submit"><Link to="/profile">ЗАРЕГИСТРИРОВАТЬСЯ</Link></button>
+                    <button className="btn" onClick={HandleForm} type="submit"><Link to="/profile">ЗАРЕГИСТРИРОВАТЬСЯ</Link></button>
                     <Link className="link" to="/login">Войти</Link>
             </form>
         </main>

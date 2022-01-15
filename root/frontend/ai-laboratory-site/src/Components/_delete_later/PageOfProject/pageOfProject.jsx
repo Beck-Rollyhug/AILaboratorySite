d4197b0c3694d585c8a906b46fb5b09ba29666ac
mybec <my.beck.for.work@mail.ru>
2022-01-15 15:07:39 +0500

    рефакторинг 1

---
+++ root/frontend/ai-laboratory-site/src/Components/_delete_later/PageOfProject/pageOfProject.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import n from "./pageOfProject.module.css";
+import HeaderOfWorkPage from "../HeaderOfWorkPage/headerOfWorkPage";
+import PageOfProjectSection from "../PageOfProjectSection/pageOfProjectSection";
+import ListOfProjectsSection from "../listOfProjectsSection/listOfProjectsSection";
+
+const PageOfProject = () => {
+  return(
+            <body className={n.body}>
+            <div className={n.profileHeader}>
+                <HeaderOfWorkPage first = "Главная" firstLink = "/"
+                                  second = "Проекты" secondLink = "/listOfProjects"
+                                  third = "Профиль" thirdLink ="/profile"
+                                  fourth = "Связаться с нами" fourthLink="/contactus"
+                                  fifth = "Выйти" fifthlink="#"
+                />
+            </div>
+            <div className={n.pageOfProjectSection}>
+                <PageOfProjectSection />
+            </div>
+            </body>
+        );
+    }
+export default PageOfProject;

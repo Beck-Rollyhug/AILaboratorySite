1534820873b3a41125a147b9adc06827415fda80
mishafominih <53509997+mishafominih@users.noreply.github.com>
2022-01-15 15:38:12 +0500

    Добавил пару функций

--- root/backend/Database/bl_funcs.py
+++ root/backend/Database/bl_funcs.py
@@ -1,4 +1,4 @@
-from Database.database import *
+from .database import *
 
 
 def add_user(data):
@@ -6,13 +6,9 @@ def add_user(data):
     Добавить пользователя в бд.
     data: Словарь -- {название поля в бд: значение}
     """
-    fields_name = '"' + '", "'.join(data.keys()) + '"'
-    fields_values = ", ".join(['%s' for key in data.values()])
-    query = f"""
-        INSERT INTO public."Users"({fields_name})
-            VALUES ({fields_values});
-    """
-    return Database.execute(query, *list(data.values()))
+    names = '"' + '", "'.join(data.keys()) + '"'
+    values = ", ".join(['%s' for val in data.values()])
+    return Database.add("Users", names, values, data.values())
 
 
 def find_user(search_filter):
@@ -20,12 +16,23 @@ def find_user(search_filter):
     Найти пользователя в бд.
     filter: Словарь -- {название поля в бд: значение}
     """
-    fltr = " and ".join([f'"{key}"=%s' for key in search_filter.keys()])
-    query = f"""
-    select * from "Users"
-    where {fltr}
+    return Database.find("Users", search_filter)
+
+
+def update_user(data):
     """
-    return Database.execute(query, *list(search_filter.values()))
+    Изменить данные о пользователе. ВАЖНО!!! Наличие поля id обязательно !!!
+    filter: Словарь -- {название поля в бд: значение}
+    """
+    return Database.update("Users", data)
+
+
+def get_user_projects():
+    pass
+
+
+def write_user_on_project(user_id, project_id):
+    Database.add("UsersProjects", '"user_id", "project_id"', '%s, %s', [user_id, project_id])
 
 
 def get_all_project():

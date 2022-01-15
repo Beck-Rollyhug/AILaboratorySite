4f4f14f30b0fbdcb8c364b4188b254dd12c130e7
mishafominih <53509997+mishafominih@users.noreply.github.com>
2022-01-15 17:12:17 +0500

    йцу

--- root/backend/Database/bl_funcs.py
+++ root/backend/Database/bl_funcs.py
@@ -1,35 +1,6 @@
 from .database import *
 
 
-def get_all_users():
-    return Database.get_all_from("Users")
-
-
-def add_user(data):
-    """
-    Добавить пользователя в бд.
-    data: Словарь -- {название поля в бд: значение}
-    """
-    names = '"' + '", "'.join(data.keys()) + '"'
-    return Database.add("Users", names, data.values())
-
-
-def find_user(search_filter):
-    """
-    Найти пользователя в бд.
-    filter: Словарь -- {название поля в бд: значение}
-    """
-    return Database.find("Users", search_filter)
-
-
-def update_user(data):
-    """
-    Изменить данные о пользователе. ВАЖНО!!! Наличие поля id обязательно !!!
-    filter: Словарь -- {название поля в бд: значение}
-    """
-    return Database.update("Users", data)
-
-
 def get_photo_partners():
     """
     Получить всех партнеров
@@ -52,13 +23,6 @@ def get_user_projects(user_id):
     return Database.execute(query)
 
 
-def get_all_news():
-    """
-    Получить все новости
-    """
-    return Database.get_all_from("Articles")
-
-
 def write_user_on_project(user_id, project_id):
     Database.add("UsersProjects", '"user_id", "project_id"', [user_id, project_id])
     Database.add("UsersProjects", '"user_id", "project_id"', [user_id, project_id])
@@ -66,30 +30,6 @@ def write_user_on_project(user_id, project_id):
 
 def del_user_on_project(user_id, project_id):
     Database.delete("UsersProjects", {"user_id":user_id, "project_id":project_id})
-
-
-def get_all_project():
-    """
-    Получить все проекты
-    """
-    return Database.get_all_from("Projects")
-
-
-def update_new(data):
-    """
-    Изменить данные о новости. ВАЖНО!!! Наличие поля id обязательно !!!
-    filter: Словарь -- {название поля в бд: значение}
-    """
-    return Database.update("Users", data)
-
-
-def add_new(data):
-    """
-    Добавить пользователя в бд.
-    data: Словарь -- {название поля в бд: значение}
-    """
-    names = '"' + '", "'.join(data.keys()) + '"'
-    return Database.add("Articles", names, data.values())
 
 
 def get_projects_for_admin():
@@ -128,4 +68,20 @@ def get_projects_for_admin():
     """
     return Database.execute(query)
 
+
+def add(table, data):
+    names = '"' + '", "'.join(data.keys()) + '"'
+    return Database.add(table, names, data.values())
+
+
+def update(table, data):
+    return Database.update(table, data)
+
+
+def delete(table, data):
+    return Database.update(table, data)
+
+
+def all(table):
+    return Database.get_all_from(table)
 
